13ebe96bd8b979a4c20368c8cac479ad8735bcee
EbtihalMaher <ebtihal.maher.d@gmail.com>
2025-07-10 14:23:49 +0300

    Token

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 
 Route::post('/login', [AuthController::class , 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user(); 
+    });
+});
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,5 +2,12 @@
 
 use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware(['auth:sanctum'])->apiResource('tickets', TicketController::class);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user(); 
+    });
+});
