9ed847482b206944886b83e53fde47a5f6555779
EbtihalMaher <ebtihal.maher.d@gmail.com>
2025-07-10 13:09:36 +0300

    Versioning

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login(){
-        return $this->ok('Hello, Login');
+
+    public function login(ApiLoginRequest $request){
+        return $this->ok($request->get('email'));
+    }
+
+    public function register(){
+        return $this->ok('register');
     }
 }
