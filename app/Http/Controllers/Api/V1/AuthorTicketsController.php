64520eb726d7de3621ad62458f3a43588ab836cc
EbtihalMaher <ebtihal.maher.d@gmail.com>
2025-07-14 11:41:59 +0300

    PUT Tickets

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\Api\V1\TicketResource;
 use App\Models\Ticket;
@@ -27,6 +28,27 @@ class AuthorTicketsController extends ApiController
 
     }
 
+    public function replace(ReplaceTicketRequest $request, $author_id, $ticket_id) {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if ($ticket->user_id == $author_id) {
+                $model = [
+                    'title' => $request->input('data.attributes.title'),
+                    'description' => $request->input('data.attributes.description'),
+                    'status' => $request->input('data.attributes.status'),
+                    'user_id' => $request->input('data.relationships.author.data.id'),
+                ];
+
+                $ticket->update($model);
+                return new TicketResource($ticket);
+            }
+            // ToDO
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket Not Found', 404);
+        }
+    }
+
     public function destroy($author_id, $ticket_id)
     {
         try {
@@ -42,4 +64,5 @@ class AuthorTicketsController extends ApiController
             return $this->error('Ticket Not Found', 404);
         }
     }
+    
 }
--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Models\Ticket;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
@@ -59,7 +60,6 @@ class TicketController extends ApiController
         } catch (ModelNotFoundException $exception) {
             return $this->error('Ticket Not Found', 404);
         }
-        
     }
 
     /**
@@ -68,6 +68,26 @@ class TicketController extends ApiController
     public function update(UpdateTicketRequest $request, Ticket $ticket)
     {
         //
+    }
+
+    public function replace(ReplaceTicketRequest $request, $ticket_id) {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            $model = [
+                'title' => $request->input('data.attributes.title'),
+                'description' => $request->input('data.attributes.description'),
+                'status' => $request->input('data.attributes.status'),
+                'user_id' => $request->input('data.relationships.author.data.id'),
+            ];
+
+            $ticket->update($model);
+
+            return new TicketResource($ticket);
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket Not Found', 404);
+        }
     }
 
     /**
